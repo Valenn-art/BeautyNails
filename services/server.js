b90357c45cd27355eddb8a549198c310c9eca8a0
Cris3p <cristianpineyro373@gmail.com>
2025-12-03 20:58:36 -0300

    puerto

--- services/server.js
+++ services/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const app = require('./App');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
     console.log(`Servidor iniciado en el puerto ${PORT}`);
