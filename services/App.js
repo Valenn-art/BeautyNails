ab41c45d5ab7b862a8715109332f996aba0f93ce
Valenn-art <valentinamh35@gmail.com>
2025-12-03 19:02:05 -0300

    w

--- services/App.js
+++ services/App.js
@@ -1,11 +1,15 @@
-const express=require('express');
+const express = require('express');
 const cors = require('cors');
+const path = require('path');           // ← AÑADIR ESTO
 const swaggerUI = require('swagger-ui-express');
 const swaggerSpec = require('./config/swaggerConfig');
 
-const app= express();
+const app = express();
 app.use(cors());
 app.use(express.json());
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname); // solo si usas ESM, sino usa path.resolve()
+app.use(express.static(path.join(__dirname, 'dist'))); // ← carpeta que genera Vite
 
 const usuarios = require('./routes/UsuariosRoutes');
 const turnos = require('./routes/TurnosRoutes');
@@ -13,16 +17,19 @@ const servicios = require('./routes/ServiciosRoutes');
 const personal = require('./routes/PersonalRoutes');
 const pago = require('./routes/PagosRoutes');
 
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec)); 
-
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
+app.use('/api', usuarios);
 app.use('/', pago);
 app.use('/', personal);
-app.use('/api', usuarios);
 app.use('/', turnos);
 app.use('/', servicios);
 
-app.get("/", (req, res) => {
-  res.send("Backend funcionando en Render 🚀");
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+app.get("/backend-test", (req, res) => {
+  res.send("Backend funcionando en Render");
 });
 
 module.exports = app;
