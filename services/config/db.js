aac6b856d2493492284b059370e1b43ba006a500
Cris3p <cristianpineyro373@gmail.com>
2025-12-04 00:23:42 -0300

    db

--- services/config/db.js
+++ services/config/db.js
@@ -4,7 +4,8 @@ const credenciales ={
     host:process.env.HOST,
     user: process.env.USER,
     password: process.env.PW,
-    database: process.env.DB
+    database: process.env.DB,
+    port: process.env.DB_PORT
 }
 
 const db = mysql.createPool(credenciales);
